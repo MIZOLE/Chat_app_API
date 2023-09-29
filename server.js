53252f27aff1641058f002e634d21e063d0e58ae
Mizole <komanalulama20@gmail.com>
2023-09-29 10:41:42 +0200

    chat api

--- server.js
+++ server.js
@@ -1,78 +1,62 @@
 const express = require("express");
-const { createServer } = require('http'); // Corrected import for createServer
-const { join } = require('path'); // Corrected import for join
-const { Server } = require('socket.io');
-//create a new app with express
 const app = express()
-const server = createServer(app);
-const io = new Server(server);
-
-
-// //Connect with the database
+const http = require('http').Server(app);
+//const { join } = require('path'); 
+const io = require('socket.io')(http, {
+    cors: { origin: '*' }
+});
 
 const cors = require("cors")
-//require the http module
-const userRouter = require("./routes/routes")
-
 app.use(cors())
-const http = require("http").Server(app)
-//require the socket.io module
-const io = require("socket.io")
-const port = 500;
-const socket = io(http);
 app.use(express.json())
 
+// app.use(express.static('public'))
 
-
-app.get("/", (req, res)=>{
-    res.send("hello")
-})
+//require the http module
+//const userRouter = require("./routes/routes")
+// app.get("/", (req, res)=>{
+//     res.send("hello")
+// })
 
 //Connect with the database
 const Chat = require("./model/chats");
 const connect = require("./db");
-const { error } = require("console");
-const router = express.Router();
+// const { error } = require("console");
+// const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, "index.html"))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(join(__dirname, "index.html"))
+// })
 
-//load a scoket io anc connect
 io.on('connection', (socket) => {
+
+    var stored_messages = []
+
     console.log('a user connected', socket.id);
 
-    // Handle incoming chat messages
-    socket.on('chat message', async (msg) => {
-        console.log('Received message:', msg);
-        const newmsg = msg
-
-        // Save messages to MongoDB
-        try {
-            const message = new Chat({ user: newmsg});
-            console.log(message)
-            
-            const result = await message.save();
-            console.log(result);
-
-            // Broadcast the message to all connected clients
-            io.emit('chat message', msg);
-        } catch (err) {
-            console.error(err);
-        }
-    });
-
-    // Listen to user disconnection
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
+    socket.on('chat message', (msg) => {
+        console.log("message", msg)
+        io.emit('chat message', `${socket.id}  ${msg}`)
+        console.log(msg)
+        //    stored_messages.push(msg, socket.id)
+        //     console.log(stored_messages)
+    })
+}
+)
 
 //wire up the server to listen to our port 500
-server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+http.listen(3000, () => {
+    console.log(`server running at port ${3000}`);
 });
 
 
 
 
+
+
+{/* <ul>
+  <li *ngFor="let message of messageList">{{ message }}</li>
+</ul>
+
+<input [(ngModel)]="newMessage" (keyup.enter)="sendMessage()" />
+<button (click)="sendMessage()">Send Message</button> */}
