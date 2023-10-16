f884c892068903b35e2cf4bceb3e6ca216855550
mfeke <mfekethothulane@gmail.com>
2023-10-16 14:11:56 +0200

    update

--- model/user.js
+++ model/user.js
@@ -6,7 +6,7 @@ const userShema = new Schema(
     {
      username: { type: String },
     password: {  type: String},
-    image:{type:String},
+    image:{type:String, default: "https://i.postimg.cc/sXQ14Hf4/no-image.webp"},
     number:{type:Number},
     email:{type:String}
      },
